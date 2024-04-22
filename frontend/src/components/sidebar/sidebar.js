86c7294fef18fc6c23ec5b28bde754e318e09908
alexmarquesrm <alexandremarquesrm@gmail.com>
2024-04-22 19:27:53 +0100

    fix no css

--- frontend/src/components/sidebar/sidebar.js
+++ frontend/src/components/sidebar/sidebar.js
@@ -6,73 +6,19 @@ import * as AiIcons from "react-icons/ai";
 import SubMenu from "./subMenu";
 import SidebarData from "./SidebarData";
 import { IconContext } from "react-icons/lib";
- 
-const Nav = styled.div`
-    background: #15171c;
-    height: 80px;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-`;
- 
-const NavIcon = styled(Link)`
-    margin-left: 2rem;
-    font-size: 2rem;
-    height: 80px;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-`;
- 
-const SidebarNav = styled.nav`
-    background: #15171c;
-    width: 250px;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    position: fixed;
-    top: 0;
-    left: ${({ sidebar }) => (sidebar ? "0" : "-100%")};
-    transition: 350ms;
-    z-index: 10;
-`;
- 
-const SidebarWrap = styled.div`
-    width: 100%;
-`;
+import './sidebar.css';
  
 const Sidebar = () => {
-    const [sidebar, setSidebar] = useState(false);
- 
-    const showSidebar = () => setSidebar(!sidebar);
- 
     return (
         <>
             <IconContext.Provider value={{ color: "#fff" }}>
-                <Nav>
-                    <NavIcon to="#">
-                        <FaIcons.FaBars
-                            onClick={showSidebar}
-                        />
-                    </NavIcon>
-                    <h1
-                        style={{
-                            textAlign: "center",
-                            marginLeft: "200px",
-                            color: "green",
-                        }}
-                    >
-                        GeeksforGeeks
-                    </h1>
-                </Nav>
-                <SidebarNav sidebar={sidebar}>
-                    <SidebarWrap>
-                        <NavIcon to="#">
-                            <AiIcons.AiOutlineClose
-                                onClick={showSidebar}
-                            />
-                            
-                        </NavIcon>
+                <nav className="SidebarNav active">
+                    <div className="SidebarWrap">
+                        <div>
+                            <p>sadas</p>
+                            <p>sadas</p>
+                            <p>sadas</p>
+                        </div>
                         {SidebarData.map((item, index) => {
                             return (
                                 <SubMenu
@@ -81,8 +27,8 @@ const Sidebar = () => {
                                 />
                             );
                         })}
-                    </SidebarWrap>
-                </SidebarNav>
+                    </div>
+                </nav>
             </IconContext.Provider>
         </>
     );

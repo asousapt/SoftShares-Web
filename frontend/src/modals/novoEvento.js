2309bcae5c042b918b8d1568e65a91928afe29c2
Alexandre Marques <alexandremarquesrm@gmail.com>
2024-05-12 02:44:00 +0100

    modal ponto de interesse e eventos

--- frontend/src/modals/novoEvento.js
+++ frontend/src/modals/novoEvento.js
@@ -32,7 +32,7 @@ const AddEventModal = ({ open, onClose }) => {
     
     return (
         <Modal open={open} onClose={onClose} >
-            <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '800px', maxWidth: '80%', maxHeight: '80%', backgroundColor: '#1D5AA1', padding: '20px' }}>
+            <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '1000px', maxWidth: '80%', maxHeight: '80%', backgroundColor: '#1D5AA1', padding: '20px' }}>
                 <h2 style={{marginTop: 0, color: 'white'}}>Novo Evento</h2>
                 <div style={{ backgroundColor: 'white', paddingLeft: 10, paddingRight: 10, paddingBottom: 20, paddingTop: 20, borderRadius: 12 }}>
                     <div style={{ marginBottom: 15 }}>
@@ -46,7 +46,7 @@ const AddEventModal = ({ open, onClose }) => {
                             <BasicTextField caption='Nº Participantes Máximo' type='number' valor={numParticipantes} onchange={(e) => setNumParticipantes(e.target.value)} />
                             <BasicTextField caption='Nº Convidados Máximo' type='number' valor={numConvidados} onchange={(e) => setNumConvidados(e.target.value)} />
                             <DataHora caption="Data e Hora" />
-                            <div style={{width: '25%'}}>
+                            <div style={{width: '30%'}}>
                                 <ComboBox caption='Subcategoria' options={opcoes} value={subcategoria} handleChange={(e) => setSubcategoria(e.target.value)} />
                             </div>
                         </div>
--- frontend/src/modals/novoPontoInt.js
+++ frontend/src/modals/novoPontoInt.js
@@ -1,34 +1,62 @@
 import React, { useState } from 'react';
 import Modal from '@mui/material/Modal';
 import BasicTextField from '../components/textFields/basic';
+import ComboBox from '../components/combobox/comboboxBasic';
+import DataHora from '../components/textFields/dataHora';
 import SubmitButton from '../components/buttons/submitButton';
 import CancelButton from '../components/buttons/cancelButton';
+import ImageTable from '../components/tables/imageTable';
 
-const AddPontoIntModal = ({ open, onClose }) => {
+const opcoes = [
+    {value: '1', label: 'teste'},
+    {value: '2', label: 'teste2'},
+];
+
+const imgs = [
+    {src: 'https://i1.sndcdn.com/artworks-000537542583-dr2w2s-t500x500.jpg', alt: 'testas'},
+    {src: 'https://i1.sndcdn.com/artworks-000537542583-dr2w2s-t500x500.jpg', alt: 'testas'},
+];
+
+const AddEventModal = ({ open, onClose }) => {
     const [title, setTitle] = useState('');
+    const [localizacao, setLocalizacao] = useState('');
     const [description, setDescription] = useState('');
+    const [numParticipantes, setNumParticipantes] = useState('');
+    const [numConvidados, setNumConvidados] = useState('');
+    const [subcategoria, setSubcategoria] = useState('');
 
     const handleAddEvent = () => {
-        console.log('Adding event:', { title, description });
-        onClose();
+        console.log('Evento Adicionado');
+        onClose(); 
     };
-
-    const handleCancel = () => {
-        onClose();
-    };
-
+    
     return (
-        <Modal open={open} onClose={onClose}>
-            <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '800px', maxWidth: '80%', maxHeight: '80%', backgroundColor: '#1D5AA1', padding: '20px' }}>
-                <h2 style={{ marginTop: 0, color: 'white' }}>Adicionar Ponto Interesse</h2>
+        <Modal open={open} onClose={onClose} >
+            <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '1000px', maxWidth: '80%', maxHeight: '80%', backgroundColor: '#1D5AA1', padding: '20px' }}>
+                <h2 style={{marginTop: 0, color: 'white'}}>Novo Ponto de Interesse</h2>
                 <div style={{ backgroundColor: 'white', paddingLeft: 10, paddingRight: 10, paddingBottom: 20, paddingTop: 20, borderRadius: 12 }}>
                     <div style={{ marginBottom: 15 }}>
-                        <div style={{ display: 'flex', gap: '10px' }}>
-                            <BasicTextField caption='Nome' valor={title} onchange={(e) => setTitle(e.target.value)} />
-                            <BasicTextField caption='Descrição' valor={description} onchange={(e) => setDescription(e.target.value)} />
+                    <div style={{display: 'flex', gap: '10px'}}>
+                        <BasicTextField caption='Titulo' valor={title} onchange={(e) => setTitle(e.target.value)} style={{ width:"600px"}}/>
+                            <div style={{width: '38%'}}>
+                                <ComboBox caption='Subcategoria' options={opcoes} value={subcategoria} handleChange={(e) => setSubcategoria(e.target.value)} />
+                            </div>
                         </div>
+                        <div style={{marginBottom: 20}}></div>
+                        <div style={{display: 'flex', gap: '10px'}}>
+                        <BasicTextField caption='Localização' valor={localizacao} onchange={(e) => setLocalizacao(e.target.value)} style={{ width:"600px"}} />
+                            <div style={{width: '38%'}}>
+                            <BasicTextField caption='Morada' valor={localizacao} onchange={(e) => setLocalizacao(e.target.value)} fullwidth={true} />
+                            </div>
+                            </div>
+                        <div style={{marginBottom: 20}}></div>
+                        <BasicTextField caption='Descrição' valor={description} onchange={(e) => setDescription(e.target.value)} fullwidth={true} />
+                        <div style={{marginBottom: 20}}></div>
+                        <BasicTextField multiline={true} caption='Descrição' valor={description} onchange={(e) => setDescription(e.target.value)} fullwidth={true}/>
+                        <div style={{marginBottom: 20}}></div>
+                        <ImageTable images={imgs} styleProp={{paddingTop: 10}} />
                     </div>
-                    <div style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
+                    <div style={{display: 'flex', justifyContent: 'center', gap: '20px'}}>
                         <CancelButton onclick={() => { onClose(); }} caption='Cancelar' />
                         <SubmitButton onclick={handleAddEvent} caption='Guardar' />
                     </div>
@@ -36,6 +64,7 @@ const AddPontoIntModal = ({ open, onClose }) => {
             </div>
         </Modal>
     );
+    
 }
 
-export default AddPontoIntModal;
+export default AddEventModal;

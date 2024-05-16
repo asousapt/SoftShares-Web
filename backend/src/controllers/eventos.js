5b544c37015b365ef476b0024e4a2006765f4094
diogofonseca01 <diogoalexcf@gmail.com>
2024-05-16 23:41:32 +0100

    rotas eventos feitas (fica a falta a dos formulario)

--- backend/src/controllers/eventos.js
+++ backend/src/controllers/eventos.js
@@ -1,7 +1,6 @@
 const { Sequelize, Op } = require('sequelize');
 const initModels = require('../models/init-models');
 const sequelizeConn = require('../bdConexao');
-const evento = require('../models/evento');
 const models = initModels(sequelizeConn);
 
 const controladorEventos = {
@@ -29,6 +28,22 @@ const controladorEventos = {
         }
     },
 
+    adicionarParticipante: async (req, res) => {
+        const { idUser, idEvento, numConvidados } = req.body;
+
+        try {
+            await models.participantes_eventos.create({
+                utilizadorid: idUser,
+                eventoid: idEvento,
+                convidadosadic: numConvidados
+            });
+            
+            res.status(201).json({ message: 'Evento adicionado com sucesso' });
+        } catch (error) {
+            res.status(500).json({ error: 'Erro ao adicionar evento' });
+        }
+    },
+
     atualizarEvento: async (req, res) => {
         const { idEvento } = req.params;
         const { titulo, descricao, dataInicio, dataFim, dataLimInscricao, nmrMaxParticipantes, localizacao, latitude, longitude, cidadeID } = req.body;
@@ -48,6 +63,43 @@ const controladorEventos = {
             }, {
                 where: {
                     eventoid: idEvento
+                }
+            });
+
+            res.status(200).json({ message: 'Evento atualizado com sucesso' });
+        } catch (error) {
+            res.status(500).json({ error: 'Erro ao atualizar evento' });
+        }
+    },
+
+    atualizarConvidados: async (req, res) => {
+        const { idUser, idEvento } = req.params;
+        const { numConvidados } = req.body;
+
+        try {
+            await models.participantes_eventos.update({
+                convidadosadic: numConvidados
+            }, {
+                where: {
+                    eventoid: idEvento,
+                    utilizadorid: idUser
+                }
+            });
+
+            res.status(200).json({ message: 'Evento atualizado com sucesso' });
+        } catch (error) {
+            res.status(500).json({ error: 'Erro ao atualizar evento' });
+        }
+    },
+
+    removerInscricao: async (req, res) => {
+        const { idUser, idEvento } = req.params;
+
+        try {
+            await models.participantes_eventos.destroy({
+                where: {
+                    eventoid: idEvento,
+                    utilizadorid: idUser
                 }
             });
 
@@ -105,20 +157,72 @@ const controladorEventos = {
     },
 
     consultarEventosEntreDatas: async (req, res) => {
-        const { poloID, data1, data2 } = req.params;
+        const { idPolo, data1, data2 } = req.params;
 
         try {
             const eventos = await models.evento.findAll({
                 where: {
                     [Op.and]: [
                         {dataInicio: {[Op.between]: [data1, data2]}},
-                        Sequelize.literal(`cidadeID IN (SELECT CIDADEID FROM POLO WHERE POLOID = ${poloID})`)
+                        Sequelize.literal(`cidadeID IN (SELECT CIDADEID FROM POLO WHERE POLOID = ${idPolo})`)
                     ]
                 }
             })
             res.status(200).json({ message: 'Consulta realizada com sucesso', data: eventos });
         } catch (error) {
             res.status(500).json({ error: 'Erro ao consultar os eventos' });
+        }
+    },
+
+    consultarUtilizadoresEvento: async (req, res) => {
+        const { idEvento } = req.params;
+
+        try{
+            const utilizadores = await models.participantes_eventos.findAll({
+                where: {
+                    eventoid: idEvento
+                }
+            })
+            res.status(200).json({ message: 'Consulta realizada com sucesso', data: utilizadores });
+        } catch (error) {
+            res.status(500).json({ error: 'Erro ao consultar os participantes' });
+        }
+    },
+
+    consultarEventoInscritos: async (req, res) => {
+        const { idUser } = req.params;
+
+        try{
+            const utilizadores = await models.participantes_eventos.findAll({
+                where: {
+                    [Op.and]: [
+                        {utilizadorid: idUser},
+                        Sequelize.literal(`dataInicio >= SYSDATE`)
+                    ]
+                }
+            })
+            res.status(200).json({ message: 'Consulta realizada com sucesso', data: utilizadores });
+        } catch (error) {
+            res.status(500).json({ error: 'Erro ao consultar os eventos' });
+        }
+    },
+
+    consultarEventosFuturos: async (req, res) => {
+        const { idPolo, numTop } = req.params;
+
+        try{
+            const utilizadores = await models.evento.findAll({
+                limit: numTop,
+                where: {
+                    [Op.and]: [
+                        Sequelize.literal(`cidadeID IN (SELECT CIDADEID FROM POLO WHERE POLOID = ${idPolo})`),
+                        Sequelize.literal(`dataInicio >= SYSDATE)`)
+                    ]
+                }
+            })
+            res.status(200).json({ message: 'Consulta realizada com sucesso', data: utilizadores });
+        } catch (error) {
+            res.status(500).json({ error: 'Erro ao consultar os participantes' });
         }
     }
 };
--- backend/src/routes/eventos.js
+++ backend/src/routes/eventos.js
@@ -5,6 +5,8 @@ const eventoControlador = require('../controllers/eventos');
 //ROUTES POST
 router.post('/add', eventoControlador.adicionarEvento);
 
+router.post('/utilizador/add', eventoControlador.adicionarParticipante);
+
 //ROUTES PUT
 router.put('/update/:idEvento', eventoControlador.atualizarEvento);
 
@@ -12,9 +14,21 @@ router.put('/cancelar/:idEvento', eventoControlador.cancelarEvento);
 
 router.put('/aprovar/:idEvento/:idUser', eventoControlador.aprovarEvento);
 
-//ROUTES GET
-router.get('/:idEvento', eventoControlador.consultarEventoPorID);
+router.put('/utilizador/update/:idEvento/:idUser', eventoControlador.atualizarConvidados);
 
-router.get('/:poloID/data/range/:data1/:data2', eventoControlador.consultarEventosEntreDatas);
+//ROUTES DELETE
+
+router.delete('/utilizador/delete/:idEvento/:idUser', eventoControlador.atualizarConvidados);
+
+//ROUTES GET
+router.get('/:idPolo/data/range/:data1/:data2', eventoControlador.consultarEventosEntreDatas);
+
+router.get('/utilizador/lista/:idEvento', eventoControlador.consultarUtilizadoresEvento);
+
+router.get('/utilizador/:idUser', eventoControlador.consultarEventoInscritos);
+
+router.get('/:idPolo/futuro/:numTop', eventoControlador.consultarEventosFuturos);
+
+router.get('/:idEvento', eventoControlador.consultarEventoPorID);
 
 module.exports = router;

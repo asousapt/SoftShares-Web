064d8bca62e36f78d2cee0206468fec6bcaa9430
Alexandre Marques <alexandremarquesrm@gmail.com>
2024-05-26 01:29:36 +0100

    users revert da query

--- backend/src/controllers/utilizadores.js
+++ backend/src/controllers/utilizadores.js
@@ -134,35 +134,33 @@ const controladorUtilizadores = {
     consultarTodos: async (req, res) => {
         try {
             const utilizadors = await sequelizeConn.query(
-                `SELECT u.*, 
-                perf.descricao AS descricao_perfil,
-                pol.descricao AS descricao_polo,
-                (SELECT valor
-                FROM traducao t 
-                    INNER JOIN chave ch ON t.chaveid = ch.chaveid 
-                        WHERE ch.registoid = u.departamentoid 
-                        AND ch.entidade = 'DEPARTAMENTO' 
-                        AND t.idiomaid = 1) AS descricao_departamento,
-                (SELECT valor
-                FROM traducao t 
-                    INNER JOIN chave ch ON t.chaveid = ch.chaveid 
-                        WHERE ch.registoid = u.funcaoid 
-                        AND ch.entidade = 'FUNCAO' 
-                        AND t.idiomaid = 1) AS descricao_funcao
+                `SELECT 
+                    u.*, 
+                    perf.descricao AS descricao_perfil,
+                    pol.descricao AS descricao_polo,
+                    dep.valor AS descricao_departamento,
+                    func.valor AS descricao_funcao
                 FROM 
                     utilizador u
                 LEFT JOIN 
                     perfil perf ON u.perfilid = perf.perfilid
                 LEFT JOIN 
-                    polo pol ON u.poloid = pol.poloid`,
+                    polo pol ON u.poloid = pol.poloid
+                INNER JOIN 
+                    chave ch_dep ON u.departamentoid = ch_dep.registoid AND ch_dep.entidade = 'DEPARTAMENTO'
+                LEFT JOIN 
+                    traducao dep ON ch_dep.chaveid = dep.chaveid AND dep.idiomaid = 1
+                INNER JOIN 
+                    chave ch_func ON u.funcaoid = ch_func.registoid AND ch_func.entidade = 'FUNCAO'
+                LEFT JOIN 
+                    traducao func ON ch_func.chaveid = func.chaveid AND func.idiomaid = 1`,
                 { type: QueryTypes.SELECT }
             );
             res.status(200).json({ message: 'Consulta realizada com sucesso', data: utilizadors });
         } catch (error) {
             res.status(500).json({ error: 'Erro ao consultar utilizadores', details: error.message });
         }
-    }
-
+    },
 };
 
 module.exports = controladorUtilizadores;

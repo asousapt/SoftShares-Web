7b22800c149c587fad615545bcf5d2ddb954267b
diogofonseca01 <diogoalexcf@gmail.com>
2024-05-26 02:07:56 +0100

    eventos filtro categorias

--- frontend/src/components/combobox/comboFilter.js
+++ frontend/src/components/combobox/comboFilter.js
@@ -2,21 +2,28 @@ import * as React from 'react';
 import MenuItem from '@mui/material/MenuItem';
 import { TextField } from '@mui/material';
 
-export default function comboBasic({ value, handleChange, options}) {
+export default function ComboBasic({ value, handleChange, options }) {
     return (
-        <TextField value={value} onChange={handleChange} size="small" style={{marginLeft: 10, backgroundColor: '#0465D9', borderRadius: '6px'}} select 
-        sx={{
-            ".MuiSelect-icon": {
-                color: 'white', 
-            },
-            ".MuiInputBase-root":{
-                color: 'white',
-            }
-        }}
+        <TextField
+            value={value}
+            size="small"
+            onChange={handleChange}
+            style={{ marginLeft: 10, backgroundColor: '#0465D9', borderRadius: '6px' }}
+            select
+            sx={{
+                ".MuiSelect-icon": {
+                    color: 'white',
+                },
+                ".MuiInputBase-root": {
+                    color: 'white',
+                }
+            }}
         >
             {options.map((option) => (
-                <MenuItem key={option.value} value={option.value}>{option.label}</MenuItem>
+                <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                </MenuItem>
             ))}
         </TextField>
-    )
+    );
 }
--- frontend/src/pages/listaEventos.js
+++ frontend/src/pages/listaEventos.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './page.css';
 /* COMPONENTES */
 import DataTable from '../components/tables/dataTable';
@@ -8,16 +9,9 @@ import AddButton from '../components/buttons/addButton';
 import Search from '../components/textFields/search';
 import ComboFilter from '../components/combobox/comboFilter';
 /* FIM COMPONENTES */
-import NovoEvento from '../modals/eventos/novoEvento';
-import axios from 'axios'; 
+import NovoEvento from '../modals/eventos/novoEvento'; 
 
 const opcoesFiltroEstado = [
-    { value: 'Todos', label: 'Todos' },
-    { value: 'Ativos', label: 'Apenas Ativos' },
-    { value: 'inativos', label: 'Apenas Inativos' }
-];
-
-const opcoesFiltroCat = [
     { value: 'Todos', label: 'Todos' },
     { value: 'Ativos', label: 'Apenas Ativos' },
     { value: 'inativos', label: 'Apenas Inativos' }
@@ -27,7 +21,8 @@ export default function ListaEventos() {
     const [isNewModalOpen, setNewModalOpen] = useState(false);
     const [filtroText, setFiltroText] = useState('');
     const [filtroEstado, setFiltroEstado] = useState('Todos');
-    const [filtroCategoria, setFiltroCategoria] = useState('Todos');
+    const [opcoesFiltroCat, setOpcoesCat] = useState([]);
+    const [filtroCategoria, setFiltroCategoria] = useState(0);
     const [tableRows, setTableRows] = useState([]);
     const [error, setError] = useState(null);
 
@@ -37,8 +32,7 @@ export default function ListaEventos() {
         { field: 'nParticipantes', headerName: '# Participantes', flex: 0.5, headerAlign: 'left' },
         { field: 'dataHora', headerName: 'Data e Hora de Começo', type: 'dateTime', width: 300, headerAlign: 'left' },
         { field: 'localizacao', headerName: 'Localização', flex: 1, headerAlign: 'left' },
-        {
-            field: 'edit', headerName: ' ', width: 90, headerAlign: 'left', sortable: false, renderCell: (row) => (
+        { field: 'edit', headerName: ' ', width: 90, headerAlign: 'left', sortable: false, renderCell: (row) => (
                 <EditButton caption=' ' /*onclick={} id={row.id}*/ />
             )
         },
@@ -55,7 +49,7 @@ export default function ListaEventos() {
                 });
                 const eventos = response.data.data;
                 setTableRows(
-                    eventos.map((evento, index) => ({
+                    eventos.map((evento) => ({
                         key: evento.eventoid,
                         id: evento.eventoid,
                         titulo: evento.titulo,
@@ -68,20 +62,31 @@ export default function ListaEventos() {
                 setError(error);
             }
         };
+        
+        const fetchCategorias = async () => {
+            try {
+                const token = 'tokenFixo';
+                const response = await axios.get('http://localhost:8000/categoria', {
+                    headers: {
+                        Authorization: `${token}`
+                    }
+                });
+                const categorias = response.data;
+                console.log(categorias);
+                setOpcoesCat([
+                    { value: 0, label: 'Sem Filtro' }, 
+                    ...categorias.map((cat) => ({
+                        value: cat.categoriaid,
+                        label: cat.valorpt
+                    }))
+                ]);
+            } catch (error) {
+                setError(error);
+            }
+        };
+        fetchCategorias();
         fetchData();
     }, []);
-
-    const handleOpenNewModal = () => {
-        setNewModalOpen(true);
-    };
-
-    const handleCloseNewModal = () => {
-        setNewModalOpen(false);
-    };
-
-    const handleTextFilter = (e) => {
-        setFiltroText(e.target.value);
-    };
 
     if (error) {
         return <div>Error: {error.message}</div>;
@@ -92,16 +97,16 @@ export default function ListaEventos() {
             <Header caption='Eventos' />
             <div className="data-container">
                 <div style={{ marginBottom: '20px', paddingTop: '20px' }}>
-                    <AddButton caption='Adicionar' onclick={handleOpenNewModal} />
-                    <Search onchange={handleTextFilter} />
+                    <AddButton caption='Adicionar' onclick={() => setNewModalOpen(true)} />
+                    <Search onchange={(e) => setFiltroText(e.target.value)} />
                     <ComboFilter options={opcoesFiltroEstado} value={filtroEstado} handleChange={(e) => setFiltroEstado(e.target.value)} />
-                    <ComboFilter options={opcoesFiltroCat} value={filtroCategoria} handleChange={(e) => setFiltroCategoria(e.target.value)} />
+                    <ComboFilter options={opcoesFiltroCat} value={filtroCategoria} handleChange={(event) => setFiltroCategoria(event.target.value)} />
                 </div>
                 <div style={{ height: '65vh', width: '99%', overflowY: 'auto', paddingBottom: '40px', border: 'none', boxShadow: 'none' }}>
                     <DataTable rows={tableRows || []} columns={tableColumns} />
                 </div>
             </div>
-            <NovoEvento open={isNewModalOpen} onClose={handleCloseNewModal} />
+            <NovoEvento open={isNewModalOpen} onClose={() => setNewModalOpen(false)} />
         </div>
     );
 }

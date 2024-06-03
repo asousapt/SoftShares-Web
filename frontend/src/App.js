711e0e9c59a8be04b7e25bd98cc2471c4a3190cd
alexmarquesrm <alexandremarquesrm@gmail.com>
2024-06-03 15:47:56 +0100

    fix sidebar em todas as paginas meno landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Sidebar from "./components/sidebar/sidebar";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from "react-router-dom";
 import ListaEventos from './pages/listaEventos';
 import ListaPontosInt from './pages/listaPontosInt';
 import ListaPublicacoes from './pages/listaPublicacoes';
@@ -21,38 +21,49 @@ import StatsReporting from './pages/estatisticasReporting';
 import Forms from './pages/forms';
 import LandingPage from './pages/landingPage';
 
-function App() {
+function AppContent() {
+    const location = useLocation();
+    const showSidebar = location.pathname !== "/landingpage";
+
     return (
-        <Router>
-            <div className="app-container">
+        <div className="app-container">
+            {showSidebar && (
                 <div className="sidebar-container">
                     <Sidebar />
                 </div>
-                <div className="content-container">
-                    <Routes>
-                        <Route path="/" element={<Navigate replace to="/landingpage" />} />
-                        <Route path="/landingpage" element={<LandingPage />} />
-                        <Route path="/dashboard" element={<Dashboard />} />
-                        <Route path="/estatisticas/utilizadores" element={<StatsUtilizador />} />
-                        <Route path="/estatisticas/denuncias" element={<StatsDenuncias />} />
-                        <Route path="/estatisticas/reporting" element={<StatsReporting />} />
-                        <Route path="/lista/eventos" element={<ListaEventos />} />
-                        <Route path="/lista/pontosinteresse" element={<ListaPontosInt />} />
-                        <Route path="/lista/publicacoes" element={<ListaPublicacoes />} />
-                        <Route path="/moderacao/aprovacoes" element={<ModAprov />} />
-                        <Route path="/moderacao/denuncias" element={<ModDen />} />
-                        <Route path="/config/polos" element={<ConfigPolos />} />
-                        <Route path="/config/forms" element={<ConfigForms />} />
-                        <Route path="/config/utilizadores" element={<ConfigUtilizadores />} />
-                        <Route path="/config/departamentos" element={<ConfigDepartamentos />} />
-                        <Route path="/config/funcoes" element={<ConfigFuncoes />} />
-                        <Route path="/config/categorias" element={<ConfigCategorias />} />
-                        <Route path="/config/subcategorias" element={<ConfigSubcategorias />} />
-                        <Route path="/config/alertas" element={<ConfigAlertas />} />
-                        <Route path="/forms" element={<Forms />} />
-                    </Routes>
-                </div>
+            )}
+            <div className="content-container">
+                <Routes>
+                    <Route path="/" element={<Navigate replace to="/landingpage" />} />
+                    <Route path="/landingpage" element={<LandingPage />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/estatisticas/utilizadores" element={<StatsUtilizador />} />
+                    <Route path="/estatisticas/denuncias" element={<StatsDenuncias />} />
+                    <Route path="/estatisticas/reporting" element={<StatsReporting />} />
+                    <Route path="/lista/eventos" element={<ListaEventos />} />
+                    <Route path="/lista/pontosinteresse" element={<ListaPontosInt />} />
+                    <Route path="/lista/publicacoes" element={<ListaPublicacoes />} />
+                    <Route path="/moderacao/aprovacoes" element={<ModAprov />} />
+                    <Route path="/moderacao/denuncias" element={<ModDen />} />
+                    <Route path="/config/polos" element={<ConfigPolos />} />
+                    <Route path="/config/forms" element={<ConfigForms />} />
+                    <Route path="/config/utilizadores" element={<ConfigUtilizadores />} />
+                    <Route path="/config/departamentos" element={<ConfigDepartamentos />} />
+                    <Route path="/config/funcoes" element={<ConfigFuncoes />} />
+                    <Route path="/config/categorias" element={<ConfigCategorias />} />
+                    <Route path="/config/subcategorias" element={<ConfigSubcategorias />} />
+                    <Route path="/config/alertas" element={<ConfigAlertas />} />
+                    <Route path="/forms" element={<Forms />} />
+                </Routes>
             </div>
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <Router>
+            <AppContent />
         </Router>
     );
 }

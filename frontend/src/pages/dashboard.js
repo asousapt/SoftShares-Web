cab085f452c3e714376d92062ebe5e7f025c2844
diogofonseca01 <diogoalexcf@gmail.com>
2024-06-06 20:06:14 +0100

    remover publicacoes das dashboards

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -6,13 +6,11 @@ import Header from '../components/header/header';
 import ChartPie from '../components/charts/chartpie';
 /* FIM COMPONENTES */
 import LocationOnIcon from '@mui/icons-material/LocationOn';
-import ForumIcon from '@mui/icons-material/Forum';
 import EventIcon from '@mui/icons-material/Event';
 import { Grid } from '@mui/material';
 
 export default function Dashboard() {
     const [numPoiAprovar, setNumPoiAprovar] = useState('13');
-    const [numPubAprovar, setNumPubAprovar] = useState('35');
     const [numEventoAprovar, setNumEventoAprovar] = useState('9');
     const [error, setError] = useState(null);
     const [data, setData] = useState([]);
@@ -52,21 +50,13 @@ export default function Dashboard() {
             <div className="data-container">
                 <div style={{ display: 'flex', textAlign: 'center', alignItems: 'center', flexDirection: 'column', paddingBottom: 20 }}>
                     <h3>Registo por Aprovar</h3>
-                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '50%', marginRight: '5%' }}>
+                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '25%', marginRight: '5%' }}>
                         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                             <div style={{ display: 'flex' }}>
                                 <LocationOnIcon />
                                 <h4 style={{ color: '#656262', margin: '0', padding: '0' }}>{numPoiAprovar}</h4>
                             </div>
                             <h4 style={{ color: 'black', margin: '0', padding: '0', marginTop: '0.5rem' }}>Pontos de Interesse</h4>
-                        </div>
-
-                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', whiteSpace: 'nowrap' }}>
-                            <div style={{ display: 'flex', alignItems: 'center' }}>
-                                <ForumIcon />
-                                <h4 style={{ color: '#656262', margin: '0', padding: '0' }}>{numPubAprovar}</h4>
-                            </div>
-                            <h4 style={{ color: 'black', margin: '0', padding: '0', marginTop: '0.5rem' }}>Publicações</h4>
                         </div>
 
                         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', whiteSpace: 'nowrap' }}>

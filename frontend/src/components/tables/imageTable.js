b05ac31f6927092858783f0cac3ba4ad39cc4d64
diogofonseca01 <diogoalexcf@gmail.com>
2024-06-15 19:33:12 +0100

    imageTable Delete button

--- frontend/src/components/tables/imageTable.js
+++ frontend/src/components/tables/imageTable.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { IconButton } from '@mui/material';
+import { IconButton, ImageListItem, ImageListItemBar } from '@mui/material';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-const ImageTable = ({ images, onAddImage, styleProp }) => {
+const ImageTable = ({ images, onAddImage, styleProp, onDelete }) => {
     return (
         <div style={styleProp}>
             <div style={{ overflowX: 'auto', display: 'flex', whiteSpace: 'nowrap' }}>
                 {images.map((image, index) => (
-                    <img key={index} src={image.src} alt={image.alt} style={{ width: 'auto', height: '200px', marginRight: '10px' }} />
+                    <ImageListItem key={index} style={{marginRight: '10px'}}>
+                        <img key={index} src={image.src} alt={image.alt} style={{ width: 'auto', height: '200px', marginRight: '10px' }} />
+                        <ImageListItemBar style={{borderRadius: 15, backgroundColor: 'transparent'}}
+                            actionIcon={
+                                <IconButton sx={{  color: 'rgba(255, 255, 255, 1)' }} onClick={onDelete} >
+                                    <DeleteIcon />
+                                </IconButton>
+                            }
+                        />
+                    </ImageListItem>
                 ))}
+                
                 <div style={{ flex: '0 0 200px', height: '200px', border: '1px solid black', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: 15 }}>
                     <IconButton onClick={onAddImage} style={{ padding: 0, color: 'black'}}>
                         <AddCircleOutlineIcon fontSize="large" color='black'/>

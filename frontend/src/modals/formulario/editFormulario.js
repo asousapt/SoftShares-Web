32d3c587128775476b3169c4f264e907eb48a2d1
Alexandre Marques <alexandremarquesrm@gmail.com>
2024-06-23 19:13:41 +0100

    small fix

--- frontend/src/modals/formulario/editFormulario.js
+++ frontend/src/modals/formulario/editFormulario.js
@@ -136,7 +136,7 @@ const EditFormulario = ({ open, onClose, idForm, setAlertOpen, setAlertProps  })
 
     return (
         <Modal open={open} onClose={handleCancel} >
-            <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '800px', maxWidth: '80%', maxHeight: '80%', backgroundColor: '#1D5AA1', padding: '20px' }}>
+            <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '800px', maxWidth: '80%', maxHeight: '80%', backgroundColor: '#1D5AA1', padding: '20px', overflow: 'auto'  }}>
                 <h2 style={{marginTop: 0, color: 'white'}}>Editar Formulario</h2>
                 <div style={{ backgroundColor: 'white', paddingLeft: 10, paddingRight: 10, paddingBottom: 20, paddingTop: 20, borderRadius: 12 }}>
                     <div style={{ marginBottom: 15 }}>
--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -148,7 +148,7 @@ export default function Dashboard() {
                             <ChartPie style={{ maxWidth: '100%' }} chartData={[]} total={0} label='Registos' />
                         </Grid>
                         <Grid item xs={12} md={4} style={{ display: 'flex', justifyContent: 'center', maxWidth: '100%' }}>
-                            <ChartPie style={{ maxWidth: '100%' }} chartData={data3} total={total3} label='Registos' />
+                            <ChartPie style={{ maxWidth: '100%' }} chartData={data3} total={total3} label='Nº Registos' />
                         </Grid>
                     </Grid>
                 </div>

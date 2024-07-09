082e54636ccd63649a36b61f66502349a395ebfb
Dio <diogomiguel.pinto2@gmail.com>
2024-07-09 15:09:47 +0100

    adicionar mobile estado inicial

--- backend/src/routes/utilizadores.js
+++ backend/src/routes/utilizadores.js
@@ -26,5 +26,6 @@ router.get('/:idUtilizador', authenticate, utilizadorControlador.consultarPorID)
 router.get('/mobile/:idUtilizador', authenticate, utilizadorControlador.consultarPorIDMobile);
 router.get('/recuperar/:id', authenticate, utilizadorControlador.recuperarPassword);
 router.get('/token/:id', utilizadorControlador.novoToken);
+router.get('/login', utilizadorControlador.login);
 
 module.exports = router;
